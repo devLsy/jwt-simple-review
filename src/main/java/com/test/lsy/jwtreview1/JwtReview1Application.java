a11cb6af30211b7a108781c1b4dc681f5a0b296b
이상엽 <infightersy@naver.com>
2025-03-14 16:40:29 +0900

    first commit~~

---
+++ src/main/java/com/test/lsy/jwtreview1/JwtReview1Application.java
@@ -0,0 +1,13 @@
+package com.test.lsy.jwtreview1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReview1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtReview1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/lsy/jwtreview1/JwtReview1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.lsy.jwtreview1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtReview1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
