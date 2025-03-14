526586ec5b4a50102f02534ad8cac41a263114a8
이상엽 <infightersy@naver.com>
2025-03-14 16:51:59 +0900

    시큐리티 설정 추가 중

--- src/main/java/com/test/lsy/jwtreview1/config/security/SecurityConfig.java
+++ src/main/java/com/test/lsy/jwtreview1/config/security/SecurityConfig.java
@@ -1,13 +1,44 @@
 package com.test.lsy.jwtreview1.config.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                // 세션 사용하지 않겠다.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // form login 사용하지 않겠다.
+                .formLogin(form -> form.disable())
+                // http 기본 인증 방식 사용하지 않겠다.
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/user/**")
+                                .hasAnyRole("USER", "MANAGER", "ADMIN")
+                            .requestMatchers("/api/v1/manager/**")
+                                .hasAnyRole("MANAGER", "ADMIN")
+                            .requestMatchers("/api/v1/admin/**")
+                                .hasAnyRole("ADMIN")
+                                .anyRequest().permitAll()
+                );
+        return http.build();
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
